b77393fd0d89289cc1d318bacad96657d4a78dfc
solidesite <solidesite@gmail.com>
2021-08-10 22:34:42 +0900

    각 기능 메소드 분리

--- src/main/Control.java
+++ src/main/Control.java
@@ -12,7 +12,7 @@ public class Control {
 	// 초기설정
 	public void start() {
 		while (true) {
-			Voca voca = new Voca();
+
 			System.out.println("--------------------");
 			System.out.println("현재 단어 목록 :");
 			for (int i = 0; i < vocaArr.size(); i++) {
@@ -23,31 +23,11 @@ public class Control {
 			System.out.println("1.단어 추가 2.단어 삭제 3.단어 수정 4.종료");
 			input = scan.nextLine();
 			if (input.equals("1")) {
-				System.out.println("추가할 영어 단어를 입력하세요.");
-				input = scan.nextLine();
-				voca.eng = input;
-				System.out.println("추가할 단어의 한글 뜻을 입력하세요");
-				input = scan.nextLine();
-				voca.kor = input;
-				vocaArr.add(voca);
-				System.out.println("추가되었습니다.");
+				create();
 			} else if (input.equals("2")) {
-				System.out.println("삭제할 단어를 선택하세요.");
-				input = scan.nextLine();
-				int num = Integer.parseInt(input);
-				vocaArr.remove(num - 1);
-				System.out.println(num + "번이 삭제되었습니다.");
+				delite();
 			} else if (input.equals("3")) {
-				System.out.println("수정할 단어를 선택하세요.");
-				input = scan.nextLine();
-				System.out.println("수정할 영어 단어를 입력하세요.");
-				String input2 = scan.nextLine();
-				System.out.println("수정할 단어의 한글 뜻을 입력하세요.");
-				String input3 = scan.nextLine();
-				int num = Integer.parseInt(input);
-				voca.eng = input2;
-				voca.kor = input3;
-				System.out.println(num + "번이 수정되었습니다.");
+				update();
 			} else if (input.equals("4")) {
 				System.out.println("프로그램을 종료합니다.");
 				break;
@@ -57,15 +37,39 @@ public class Control {
 
 	// 단어 추가
 	public void create() {
+		Voca voca = new Voca();
+		System.out.println("추가할 영어 단어를 입력하세요.");
+		input = scan.nextLine();
+		voca.eng = input;
+		System.out.println("추가할 단어의 한글 뜻을 입력하세요");
+		input = scan.nextLine();
+		voca.kor = input;
+		vocaArr.add(voca);
+		System.out.println("추가되었습니다.");
+	}
 
-	}
-	
-	//단어 삭제
+	// 단어 삭제
 	public void delite() {
-		
+		System.out.println("삭제할 단어를 선택하세요.");
+		input = scan.nextLine();
+		int num = Integer.parseInt(input);
+		vocaArr.remove(num - 1);
+		System.out.println(num + "번이 삭제되었습니다.");
 	}
-	
-	//단어 수정
-	public void update() {}
+
+	// 단어 수정
+	public void update() {
+		System.out.println("수정할 단어를 선택하세요.");
+		input = scan.nextLine();
+		System.out.println("수정할 영어 단어를 입력하세요.");
+		String input2 = scan.nextLine();
+		System.out.println("수정할 단어의 한글 뜻을 입력하세요.");
+		String input3 = scan.nextLine();
+		int num = Integer.parseInt(input) - 1;
+		Voca voca = vocaArr.get(num);
+		voca.eng = input2;
+		voca.kor = input3;
+		System.out.println(num + "번이 수정되었습니다.");
+	}
 
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -4,10 +4,8 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-
 	public static void main(String[] args) {
 		Control con = new Control();
 		con.start();
 	}
-
 }
